dd5eb43f2186d78e8bf99004950cd06fc80100ff
Peter Gustafson <peterlideckergustafson@gmail.com>
2018-02-13 16:06:09 -0600

    updates to js file, crazy bug fix...

--- main.js
+++ main.js
@@ -4,55 +4,83 @@ $(document).ready(function () {
     // Initial array of Sports to Populate Initial Buttons
     var topics = ["baseball", "basketball", "football", "hockey", "golf", "soccer", "lacrosse", "curling"];
 
-    //Function to Display Search Query when user clicks button
-    function getTopic (topic) {
-        //Get topic from topics array
-    var index = topics.indexOf(topic);
-    return encodeURIComponent (topics[index]);
-    };    
-    
+    //Function to Display Search Query when User Clicks Button
+    function getTopic(topic) {
+        //Get Topic from Topics Array
+        var index = topics.indexOf(topic);
+        //This allows user to search using multiple words/strings
+        return encodeURIComponent(topics[index]);
+    };
+
     // Function for dumping the JSON content for each button into the div
     function displaySportInfo() {
 
         var topic = $(this).attr("data-name");
-        //http://api.giphy.com/v1/gifs/search?api_key=9HeQ9HW3dHXdNkyJQDA69pPw4TvgtlG0&q=baseball&limit=10
         var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=9HeQ9HW3dHXdNkyJQDA69pPw4TvgtlG0&q=" + getTopic(topic) + "&limit=10";
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).
-        
-        then(function (response) {
-            $("#sports-gifs").empty();
-            for (var i = 0; i < response.data.length; i++) {
 
-            // Creating a div to hold the sports gif
-            var gifDiv = $("<div class='sport'>");
+            then(function (response) {
+                // Deleting the sports gifs div prior to adding gifs to avoid repeat gif images
+                $("#sports-gifs").empty();
+                //For loop to run through all responses (10) from API request for each search
+                for (var i = 0; i < response.data.length; i++) {
 
-            // Storing the rating data
-            var rating = response.data[i].rating;
-            console.log(rating);
-            // Creating an element to have the rating displayed
-            var gifrating = $("<p>").text("Rating: " + rating);
-            // Displaying the rating
-            gifDiv.append(gifrating);
+                    // Creating a div to hold the sports gif
+                    var gifDiv = $("<div class='sportdiv'>");
 
-            // Retrieving the URL for the gif image
-            var imgURL = response.data[i].images.original.url;
+                    // Storing the rating data
+                    var rating = response.data[i].rating;
 
-            // Creating an element to hold the image
-            var image = $("<img>").attr("src", imgURL);
+                    // Creating an element to have the rating displayed
+                    var gifrating = $("<p>").text("Rating: " + rating);
 
-            // Appending the Sports gif image
-            gifDiv.append(image);
+                    // Displaying the rating
+                    gifDiv.append(gifrating);
 
-            // Putting the entire sports gif above the previous sports gifs
-            $("#sports-gifs").append(gifDiv);
+                    //Variable for Images
+                    var image = $("<img>");
+                    image.addClass("sport");
+
+                    // Retrieving the URL for the static gif image (default)
+                    var imgURL = response.data[i].images.fixed_height_still.url;
+
+                    //Retrieve the URL for the animated gif image & apply to data animate class
+                    image.attr("data-animate", response.data[i].images.fixed_height.url);
+
+                    // Retrieving the URL for the static gif image & apply to data still class
+                    image.attr("data-still", response.data[i].images.fixed_height_still.url);
+
+                    // Creating an element to hold the image & apply data-still/default class 
+                    image.attr("src", response.data[i].images.fixed_height_still.url);
+
+                    //Adding Data State Still to all gif images upon adding them to div
+                    image.attr("data-state", "still");
+
+                    // Appending the Sports gif image
+                    gifDiv.append(image);
+
+                    // Putting the entire sports gif above the previous sports gifs
+                    $("#sports-gifs").append(gifDiv);
+
+                };
+            });
+    };
+
+    //On Click Function to Animate/Pause Gifs 
+    $(document).on("click", ".sport", function () {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animated");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
         }
-        });
-
-    }
+    });
 
     // Function for displaying all sports buttons
     function renderButtons() {
@@ -63,7 +91,7 @@ $(document).ready(function () {
         // Looping through the pre-defined array of sports
         for (var i = 0; i < topics.length; i++) {
 
-            // Here we're dynamically generating buttons for each sport in the array.
+            // Dynamically generating buttons for each sport in the array.
             var a = $("<button>");
             // Adding a class for each button
             a.addClass("sport-btn");
@@ -74,17 +102,18 @@ $(document).ready(function () {
             // Adding the button to the HTML in the Sports-Buttons div
             $("#sports-buttons").append(a);
         }
-    }
+    };
 
     // This function handles events when the submit button is clicked by the user
     $("#add-giphy").on("click", function (event) {
+
         // event.preventDefault prevents the form from submitting itself. Also allows user to hit enter instead of clicking button
         event.preventDefault();
 
-        // This line will grab the input text from the input box
+        // Grabs the input text from the input box
         var sportInput = $("#giphy-input").val().trim();
 
-        // The movie from the textbox is then added to our array
+        // The sport search from the textbox is then added to our array
         topics.push(sportInput);
 
         // calling renderButtons function which handles the processing of our sports array
@@ -92,76 +121,10 @@ $(document).ready(function () {
     });
 
     // Function for displaying the Sports giphy info
-    // Using $(document).on instead of $(".sport").on to add event listeners to dynamically generated elements
     $(document).on("click", ".sport-btn", displaySportInfo);
 
     // Calling the renderButtons function at least once to display the initial list of sports
     renderButtons();
-
-
-
-
-
-
-
-
-
-
-    //For Loop to Go through and load all Pre-Defined Buttons on click (load 10 results, show )
-
-    // var api = "http://api.giphy.com/v1/gifs/search?";
-    // var apiKey = "&api_key=9HeQ9HW3dHXdNkyJQDA69pPw4TvgtlG0";
-    // var query = "&q=baseball";
-
-    // $.ajax({
-    //     type: 'GET',
-    //     url: "http://api.giphy.com/v1/gifs/search?&api_key=9HeQ9HW3dHXdNkyJQDA69pPw4TvgtlG0&q=baseball",
-    //     success: function(data) {
-    //         console.log('success', data);
-    //         $.each(data, function(i, gif) {
-    //             $('#sports-buttons').append('<img src=' + url + '>')
-    //         })
-    //     }
-    // })
-
-
-    //From Giphy Youtube Video
-    // var api = "http://api.giphy.com/v1/gifs/search?";
-    // var apiKey = "&api_key=9HeQ9HW3dHXdNkyJQDA69pPw4TvgtlG0";
-    // var query = "&q=baseball";
-
-    // function setup() {
-    //     noCanvas();
-    //     var url = api + apiKey + query;
-    //     loadJSON(url, gotData);
-    // };
-
-    // function gotData (giphy) {
-    //     for (i = 0; i < giphy.data.length; i++) {
-    //     createImg(giphy.data[i].images.original.url);
-    //   }
-    // };
-
-
-    //Test from Giphy API documentation 
-    // var baseball = $.get("http://api.giphy.com/v1/gifs/search?q=baseball&api_key=9HeQ9HW3dHXdNkyJQDA69pPw4TvgtlG0&limit=10");
-    // baseball.done(function(data) { 
-    //     console.log("success got data", data); });
-
-    //Function to listen for Click Action for the "Add Sport" button to dynamically add a new button with user's search query
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     //End Document Ready Function
 });
